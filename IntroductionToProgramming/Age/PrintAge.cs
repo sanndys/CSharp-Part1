fb2e4da4b0241b4aa23c979ea1c0ce49688e2283
Boris Nedev <borisnedev@abv.bg>
2016-04-06 00:14:33 +0300

    full homework

---
+++ IntroductionToProgramming/Age/PrintAge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Age 
+{
+    class PrintAge
+    {
+        static void Main()
+        {
+            DateTime currentDate = DateTime.Now;
+
+            string input = (Console.ReadLine());
+            string dateFormat = "MM'.'dd'.'yyyy";
+            DateTime birthDate = DateTime.ParseExact(input, dateFormat, CultureInfo.InvariantCulture);
+
+            int age = (int)((currentDate - birthDate).TotalDays / 365.25);
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+        }
+    }
+}
